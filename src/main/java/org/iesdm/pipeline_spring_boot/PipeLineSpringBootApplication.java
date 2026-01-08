75173730c2a76f56012ef00a595608e96a4a3406
HerrecuDev <phercue2805>
2026-01-08 21:32:07 +0100

    prueba

--- src/main/java/org/iesdm/pipeline_spring_boot/PipeLineSpringBootApplication.java
+++ src/main/java/org/iesdm/pipeline_spring_boot/PipeLineSpringBootApplication.java
@@ -8,6 +8,7 @@ public class PipeLineSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PipeLineSpringBootApplication.class, args);
+        
     }
 
 }
